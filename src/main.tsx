6e26bb7cfcbf41b2180b6ba9508bc0d3909a9d1a
veronikavatsyak <veronikateslya@ukr.net>
2025-03-18 08:41:57 -0400

    correcting error

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import App from "./App";
 import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
